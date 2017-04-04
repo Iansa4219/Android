3627bda58b9102b423f4ef402379f4fb052a3394
Al-Imran123 <galimren@gmail.com>
2017-04-04 19:08:59 +0600

    First commit #2

---
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/FragmentOne.java
@@ -0,0 +1,27 @@
+package com.example.myfirstapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by al-imranislam on 3/7/17.
+ */
+
+public class FragmentOne extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_on_layout, container, false);
+
+
+
+
+
+    }
+}
